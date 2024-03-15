dd203b2f183a4c55d255ce0e0a49b408b038eec7
Alessio Petrin <alessiopetrin9@gmail.com>
2024-03-15 09:06:42 +0000

    Setup tools

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+const requiredEnvVars = [
+  "AWS_REGION",
+  "AWS_ACCESS_KEY_ID",
+  "AWS_SECRET_ACCESS_KEY",
+  "AWS_S3_BUCKET_NAME",
+];
+
+// Ensure all env variables are set before executing app
+if (process.env.NODE_ENV !== "test") {
+  requiredEnvVars.forEach((key) => {
+    if (!process.env[key]) {
+      throw new Error(`Environment variable ${key} is not set`);
+    }
+  });
+}
+
 const nextConfig = {};
 
 export default nextConfig;
